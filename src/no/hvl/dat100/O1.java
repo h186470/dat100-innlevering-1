e4e39dddf37c7f61deb80f223faa60528aa62a7f
sebjor98 <sebastian.bjornhom@gmail.com>
2024-09-06 11:59:39 +0200

    implemented O1 and O2

--- src/no/hvl/dat100/O1.java
+++ src/no/hvl/dat100/O1.java
@@ -1,10 +1,57 @@
 package no.hvl.dat100;
 
+import static javax.swing.JOptionPane.*;
+
 public class O1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        String bruttoInntektS = showInputDialog("Hva er din bruttoinntekt?");
+        double bruttoinntekt = Double.parseDouble(bruttoInntektS);
+        
+        // Trinnskatten inntektsnivåer
+        double trinn0I = 208050;
+        double trinn1I = 292850;
+        double trinn2I = 670000;
+        double trinn3I = 937900;
+        double trinn4I = 1350000;
+        
+        // Trinnskatten prosentsatser omgjort til desimaler
+        double trinn1P = 0.017;
+        double trinn2P = 0.040;
+        double trinn3P = 0.136;
+        double trinn4P = 0.166;
+        double trinn5P = 0.176;
+        
+        double skatteTrekk = 0;
+        
+        String skatteTrinn = "";
 
-	}
+        if (bruttoinntekt > trinn4I) {
+            skatteTrekk += (bruttoinntekt - trinn4I) * trinn5P;
+            bruttoinntekt = trinn4I;
+            skatteTrinn = "5";
+        }
+        if (bruttoinntekt > trinn3I) {
+            skatteTrekk += (bruttoinntekt - trinn3I) * trinn4P;
+            bruttoinntekt = trinn3I;
+            skatteTrinn = "4";
+        }
+        if (bruttoinntekt > trinn2I) {
+            skatteTrekk += (bruttoinntekt - trinn2I) * trinn3P;
+            bruttoinntekt = trinn2I;
+            skatteTrinn = "3";
+        }
+        if (bruttoinntekt > trinn1I) {
+            skatteTrekk += (bruttoinntekt - trinn1I) * trinn2P;
+            bruttoinntekt = trinn1I;
+            skatteTrinn = "2";
+        }
+        if (bruttoinntekt > trinn0I) {
+            skatteTrekk += (bruttoinntekt - trinn0I) * trinn1P;
+            skatteTrinn = "1";
+        }
+
+        System.out.println("Du er på skattetrinn " + skatteTrinn +", ditt skattetrekk er derfor: " + skatteTrekk);
+    }
 
 }
--- src/no/hvl/dat100/O2.java
+++ src/no/hvl/dat100/O2.java
@@ -1,9 +1,49 @@
 package no.hvl.dat100;
 
+import static javax.swing.JOptionPane.showInputDialog;
+import static javax.swing.JOptionPane.showMessageDialog;
+
 public class O2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String antallEleverS = showInputDialog("Hvor mange elever skal graderes?");
+		int antallElever = Integer.parseInt(antallEleverS);
+
+		for (int i = 0; i < antallElever; i++) {
+			String innlestSumS = showInputDialog("Hvor mange poeng fikk eleven?");
+			int innlestSum = Integer.parseInt(innlestSumS);
+			int h = 100;
+			int l = 0;
+
+			if (innlestSum < l || innlestSum > h) {
+				showMessageDialog(null, "Ugyldig verdi, poengsum er mellom 0 og 100.");
+				i--;
+				innlestSum = -1;
+			}
+			String karakter;
+
+			if (innlestSum <= 39 && innlestSum > l) {
+				karakter = "F";
+				showMessageDialog(null, "Eleven har fått karakteren " + karakter + " med " + innlestSum + " poeng.");
+			} else if (innlestSum <= 49 && innlestSum > 39) {
+				karakter = "E";
+				showMessageDialog(null, "Eleven har fått karakteren " + karakter + " med " + innlestSum + " poeng.");
+			} else if (innlestSum <= 59 && innlestSum > 49) {
+				karakter = "D";
+				showMessageDialog(null, "Eleven har fått karakteren " + karakter + " med " + innlestSum + " poeng.");
+			} else if (innlestSum <= 79 && innlestSum > 59) {
+				karakter = "C";
+				showMessageDialog(null, "Eleven har fått karakteren " + karakter + " med " + innlestSum + " poeng.");
+			} else if (innlestSum <= 80 && innlestSum > 79) {
+				karakter = "B";
+				showMessageDialog(null, "Eleven har fått karakteren " + karakter + " med " + innlestSum + " poeng.");
+			} else if (innlestSum <= h && innlestSum > 89) {
+				karakter = "A";
+				showMessageDialog(null, "Eleven har fått karakteren " + karakter + " med " + innlestSum + " poeng.");
+			} else if (innlestSum == -1) {
+				showMessageDialog(null, "Tast inn poengsum på nytt");
+			}
+		}
 
 	}
 
