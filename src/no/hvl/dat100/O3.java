16f6c3e71df6f30aea4b6fd162f389d9ac75a800
sebjor98 <sebastian.bjornhom@gmail.com>
2024-09-09 16:48:06 +0200

    Added O3

--- src/no/hvl/dat100/O3.java
+++ src/no/hvl/dat100/O3.java
@@ -1,9 +1,33 @@
 package no.hvl.dat100;
 
+import static javax.swing.JOptionPane.*;
+
 public class O3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Leser inn et heltall n > 0 ved hjelp av en dialogboks
+        String input = showInputDialog("Skriv inn et heltall n (n > 0):");
+
+        // Sjekker om input er et gyldig heltall
+        if (input != null && input.matches("\\d+")) {
+            int n = Integer.parseInt(input);
+
+            if (n <= 0) {
+                showMessageDialog(null, "Vennligst skriv inn et heltall større enn 0.");
+            } else {
+                // Beregner verdien av n!
+                int faktorielle = 1;
+                for (int i = 1; i <= n; i++) {
+                    faktorielle *= i;
+                }
+
+                // Viser verdien av n! i en dialogboks
+                showMessageDialog(null, "Verdien av " + n + "! er: " + faktorielle);
+            }
+        } else {
+            // Viser en feilmelding hvis input ikke er et gyldig heltall
+            showMessageDialog(null, "Ugyldig input. Vennligst skriv inn et positivt heltall.");
+        }
 
 	}
 
